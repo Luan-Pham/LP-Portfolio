7a1d6b92fb917264a1d6e21cddfc7285a0570fa2
unknown <phamexb@gmail.com>
2023-01-29 23:04:16 -0800

    created scroll and active states

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,95 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect } from 'react';
 
 function NavBar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
       <Container>
         <Navbar.Brand href="#home">Luan Pham</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Skills</Nav.Link>
-            <Nav.Link href="#link">Resume</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('home')}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={
+                activeLink === 'about' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('about')}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={
+                activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('projects')}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={
+                activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('skills')}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={
+                activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('resume')}
+            >
+              Resume
+            </Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://github.com/Luan-Pham">
+                <img src={''} alt="GitHub logo" />
+              </a>
+              <a href="https://www.linkedin.com/in/luan-pham3/">
+                <img src={''} alt="GitHub logo" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}>
+              <span>Contact Me</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
