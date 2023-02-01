698130b2d6c1859c4df456c0b4d3bf9255300dca
unknown <phamexb@gmail.com>
2023-02-01 02:26:26 -0800

    stuck at cards for now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import React, { useState } from 'react';
 import { Container, Card } from 'react-bootstrap';
+import '@splidejs/react-splide/css';
+import Thyme from '../assets/Thyme.jpg';
 
 function Projects() {
   const projects = [
@@ -8,12 +10,9 @@ function Projects() {
       title: 'Thyme2Cook',
       description:
         'Latest application that I am working on! Ap app to help you decide to what to make for dinner using the spoonacular API.',
-      imgUrl: '',
-      link: '',
+      imgUrl: { Thyme },
+      link: 'https://luan-pham.github.io/Thyme2Cook/',
     },
-    { title: '', description: '', imgUrl: '', link: '' },
-    { title: '', description: '', imgUrl: '', link: '' },
-    { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
@@ -21,15 +20,32 @@ function Projects() {
   ];
 
   return (
-    <Container className="slideContainer">
+    <Container
+      options={{
+        rewind: true,
+        gap: '2rem',
+        perPage: 4,
+        pagination: false,
+        drag: 'free',
+        breakpoints: {
+          2050: { perPage: 3 },
+          1500: { perPage: 2 },
+          640: {
+            perPage: 1,
+          },
+        },
+      }}
+      className="slideContainer"
+    >
       <h2> Check out some of my projects!</h2>
       <Splide>
-        {projects.map((project) => {
+        {projects.map((project, index) => {
           return (
             <SplideSlide>
-              <Card className="splideCard">
+              <Card className="splideCard" key={index}>
                 <h1>{project.title}</h1>
                 <p>{project.description}</p>
+                <img src={project.imgUrl} alt={project.title}></img>
               </Card>
             </SplideSlide>
           );
