b2352659cce44c0197cb5e0d3286a30e6dd38649
unknown <phamexb@gmail.com>
2023-02-01 01:22:07 -0800

    added projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Projects() {
+  const projects = [
+    {
+      title: 'Thyme2Cook',
+      descrption:
+        'Latest application that I am working on! Ap app to help you decide to what to make for dinner using the spoonacular API.',
+      imgUrl: '',
+    },
+  ];
+
+  return <div>Projects</div>;
+}
+
+export default Projects;
