cee1c32b0b34df7c7041d35522889a2482f51e3d
unknown <phamexb@gmail.com>
2023-02-01 00:56:56 -0800

    created About section

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/About';
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <About />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+function About() {
+  return <div>About</div>;
+}
+
+export default About;
