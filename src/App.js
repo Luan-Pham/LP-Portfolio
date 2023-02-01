c77139ca002af9d788e436cd19276997f3ad1280
unknown <phamexb@gmail.com>
2023-02-01 01:24:55 -0800

    installed splide

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
       <NavBar />
       <About />
+      <Projects />
     </div>
   );
 }
