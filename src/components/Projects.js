f45b683c22207a2a02f5b366da7a6be0a171b7f8
unknown <phamexb@gmail.com>
2023-02-01 01:48:08 -0800

    render splide?

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import React, { useState } from 'react';
+import { Container, Card } from 'react-bootstrap';
 
 function Projects() {
   const projects = [
     {
       title: 'Thyme2Cook',
-      descrption:
+      description:
         'Latest application that I am working on! Ap app to help you decide to what to make for dinner using the spoonacular API.',
       imgUrl: '',
+      link: '',
     },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
   ];
 
-  return <div>Projects</div>;
+  return (
+    <Container className="slideContainer">
+      <h2> Check out some of my projects!</h2>
+      <Splide>
+        {projects.map((project) => {
+          return (
+            <SplideSlide>
+              <Card className="splideCard">
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+              </Card>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Container>
+  );
 }
 
 export default Projects;
+
+// style="margin-right: 2rem; width: calc(((100% + 2rem) / 3) - 2rem); height: 10rem;"
