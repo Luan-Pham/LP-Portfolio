e67f1c2952866c18562a117e0f328b60c1ed03be
unknown <phamexb@gmail.com>
2023-02-01 19:40:09 -0800

    changed to cards instead of splide

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,5 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import React, { useState } from 'react';
-import { Container, Card } from 'react-bootstrap';
-import '@splidejs/react-splide/css';
+import React from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
 import Thyme from '../assets/Thyme.jpg';
 
 function Projects() {
@@ -10,47 +8,30 @@ function Projects() {
       title: 'Thyme2Cook',
       description:
         'Latest application that I am working on! Ap app to help you decide to what to make for dinner using the spoonacular API.',
-      imgUrl: { Thyme },
+      imgUrl: Thyme,
       link: 'https://luan-pham.github.io/Thyme2Cook/',
     },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
+    { title: '', description: '', imgUrl: '', link: '' },
   ];
-
   return (
-    <Container
-      options={{
-        rewind: true,
-        gap: '2rem',
-        perPage: 4,
-        pagination: false,
-        drag: 'free',
-        breakpoints: {
-          2050: { perPage: 3 },
-          1500: { perPage: 2 },
-          640: {
-            perPage: 1,
-          },
-        },
-      }}
-      className="slideContainer"
-    >
+    <Container>
       <h2> Check out some of my projects!</h2>
-      <Splide>
-        {projects.map((project, index) => {
-          return (
-            <SplideSlide>
-              <Card className="splideCard" key={index}>
-                <h1>{project.title}</h1>
-                <p>{project.description}</p>
-                <img src={project.imgUrl} alt={project.title}></img>
-              </Card>
-            </SplideSlide>
-          );
-        })}
-      </Splide>
+      {projects.map((project, index) => (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={project.imgUrl} />
+          <Card.ImgOverlay>
+            <Card.Body>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text>{project.description}</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card.ImgOverlay>
+        </Card>
+      ))}
     </Container>
   );
 }
