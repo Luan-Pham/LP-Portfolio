aae57109821964f32086c8470e99fb0b95b27957
unknown <phamexb@gmail.com>
2023-02-01 19:57:52 -0800

    grid layout for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Card, Button } from 'react-bootstrap';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import { useBootstrapBreakpoints } from 'react-bootstrap/esm/ThemeProvider';
 import Thyme from '../assets/Thyme.jpg';
 
 function Projects() {
@@ -11,8 +12,19 @@ function Projects() {
       imgUrl: Thyme,
       link: 'https://luan-pham.github.io/Thyme2Cook/',
     },
-    { title: '', description: '', imgUrl: '', link: '' },
-    { title: '', description: '', imgUrl: '', link: '' },
+    {
+      title: 'Barcade',
+      description:
+        'Group project collaborating with other sudents in bootcamp. A virtual bar and arcade platform used to socialize with friends, play games, and set high scores!',
+      imgUrl: '',
+      link: 'https://secure-everglades-09731.herokuapp.com/',
+    },
+    {
+      title: 'Marvel Search',
+      description: `Collaborative project to use the marvel API in order to serach for information about its expansive universe. Users can serach for data about a character's backstory, comic books, and movies`,
+      imgUrl: '',
+      link: 'https://luan-pham.github.io/Thyme2Cook/',
+    },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
     { title: '', description: '', imgUrl: '', link: '' },
@@ -20,18 +32,28 @@ function Projects() {
   return (
     <Container>
       <h2> Check out some of my projects!</h2>
-      {projects.map((project, index) => (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={project.imgUrl} />
-          <Card.ImgOverlay>
-            <Card.Body>
-              <Card.Title>{project.title}</Card.Title>
-              <Card.Text>{project.description}</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card.ImgOverlay>
-        </Card>
-      ))}
+      <Row>
+        {projects.map((project, index) => (
+          <Col xs={6} md={4}>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '3rem',
+                padding: '1px',
+              }}
+            >
+              <Card.Img variant="top" src={project.imgUrl} />
+              <Card.ImgOverlay>
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
