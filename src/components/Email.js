a4341017f01528567b3efd702212bb9852efd702
unknown <phamexb@gmail.com>
2023-02-05 14:37:17 -0800

    basic form layout

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 function Email() {
   const form = useRef();
@@ -30,12 +30,17 @@ function Email() {
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="name" placeholder="Your Name" />
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Email Address" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={5} />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
     </Container>
   );
