817a069cc6edb2966cad2e87f155f12c741e0a8a
Luan Pham <phamexb@gmail.com>
2023-02-06 16:41:15 -0800

    added skills list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Container, ListGroup } from "react-bootstrap";
+import { FaNodeJs, FaReact, FaHtml5, FaCss3 } from "react-icons/fa";
 
 function Skills() {
-  return <div>Skills</div>;
+  const skills = [
+    { skill: "HTML", icon: FaHtml5 },
+    { skill: "CSS", icon: FaCss3 },
+    { skill: "Node.js", icon: FaNodeJs },
+    { skill: "React", icon: FaReact },
+  ];
+
+  return (
+    <div>
+      <Container>
+        <ListGroup>
+          {skills.map((skill) => (
+            <ListGroup.Item>
+              {skill.skill} <icon as={skill.icon}></icon>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Container>
+    </div>
+  );
 }
 
 export default Skills;
