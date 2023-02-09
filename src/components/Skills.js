d4021ee6e93033ed461f48d743b323e386e9454b
unknown <phamexb@gmail.com>
2023-02-08 19:15:31 -0800

    skill cards

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { Container, ListGroup } from 'react-bootstrap';
-import { FaNodeJs, FaReact, FaCss3 } from 'react-icons/fa';
-import ReactOriginalIcon from 'react-devicons/react/original';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import ReactOriginalWordmarkIcon from 'react-devicons/react/original-wordmark';
+import Css3OriginalWordmarkIcon from 'react-devicons/css3/original-wordmark';
+import NodejsOriginalWordmarkIcon from 'react-devicons/nodejs/original-wordmark';
+import Html5OriginalWordmarkIcon from 'react-devicons/html5/original-wordmark';
+import JqueryOriginalIcon from 'react-devicons/jquery/original';
+import MongodbOriginalWordmarkIcon from 'react-devicons/mongodb/original-wordmark';
+import MysqlOriginalWordmarkIcon from 'react-devicons/mysql/original-wordmark';
+import BootstrapPlainWordmarkIcon from 'react-devicons/bootstrap/plain-wordmark';
+import GitOriginalIcon from 'react-devicons/git/original';
 
 function Skills() {
   const skills = [
-    { skill: 'HTML', icon: <ReactOriginalIcon /> },
-    { skill: 'CSS', icon: FaCss3 },
-    { skill: 'Node.js', icon: FaNodeJs },
-    { skill: 'React', icon: FaReact },
+    { skill: 'HTML', icon: <Html5OriginalWordmarkIcon /> },
+    { skill: 'CSS', icon: <Css3OriginalWordmarkIcon /> },
+    { skill: 'Node.js', icon: <NodejsOriginalWordmarkIcon /> },
+    { skill: 'React', icon: <ReactOriginalWordmarkIcon /> },
+    { skill: 'jQuery', icon: <JqueryOriginalIcon /> },
+    { skill: 'MongoDB', icon: <MongodbOriginalWordmarkIcon /> },
+    { skill: 'MySQL', icon: <MysqlOriginalWordmarkIcon /> },
+    { skill: 'Bootstrap', icon: <BootstrapPlainWordmarkIcon /> },
+    { skill: 'Git', icon: <GitOriginalIcon /> },
   ];
 
   return (
-    <div>
-      <Container>
-        <ListGroup>
-          {skills.map((skill) => (
-            <ListGroup.Item>
-              {skill.skill}
-              {skill.icon}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Container>
-    </div>
+    <Container id="skills">
+      <Row>
+        {skills.map((skill) => (
+          <Col>
+            <Card style={{ width: '18rem', margin: '1rem' }}>
+              <Card.Body>
+                <Card.Header
+                  style={{
+                    color: 'black',
+                    textAlign: 'center',
+                    fontWeight: '500',
+                    fontSize: '2rem',
+                  }}
+                >
+                  {skill.skill} {skill.icon}
+                </Card.Header>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
